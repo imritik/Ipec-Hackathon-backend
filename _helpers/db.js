79f6f75afa920308756e7f2fa5ca2c55b3b484c3
RITIK VERMA <ritikvverma@gmail.com>
2018-09-09 11:58:51 +0530

    Update db.js

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://ipec:ipec2018@ds249092.mlab.com:49092/ipec");
+mongoose.connect("mongodb://username:password@ds249092.mlab.com:49092/ipec");
 mongoose.Promise = global.Promise;
 
 module.exports = {
